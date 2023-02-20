d4becf7bc491b26f76738f7a4d856a2389a20fb4
Olgu Örs Başak <olgubasak@hacettepe.edu.tr>
2023-02-20 16:09:26 +0300

    Add custom input

--- src/main/java/org/primefaces/test/UIGreet.java
+++ src/main/java/org/primefaces/test/UIGreet.java
@@ -2,12 +2,18 @@ package org.primefaces.test;
 
 import java.io.IOException;
 
+import javax.faces.application.ResourceDependencies;
+import javax.faces.application.ResourceDependency;
 import javax.faces.component.FacesComponent;
 import javax.faces.component.UIOutput;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
 
 @FacesComponent("UIGreet")
+@ResourceDependencies({
+    @ResourceDependency(name = "test.js"),
+    @ResourceDependency(name = "test.css")
+})
 public class UIGreet extends UIOutput {
     public UIGreet() {
         setRendererType(null);
@@ -16,8 +22,9 @@ public class UIGreet extends UIOutput {
     protected enum PropertyKeys {
         greetText,
         size,
+        style,
         styleClass,
-        onclick,
+        onclick
     }
 
     public String getGreetText() {
@@ -28,57 +35,60 @@ public class UIGreet extends UIOutput {
         getStateHelper().put(PropertyKeys.greetText, greetText);
     }
 
+    public String getStyle() {
+        return (String) getStateHelper().eval(PropertyKeys.style, null);
+    }
+
+    public void setStyle(String style) {
+        getStateHelper().put(PropertyKeys.style, style);
+    }
+
     public String getSize() {
-        return (String) getStateHelper().eval(PropertyKeys.size, null);
+        return (String) getStateHelper().eval(PropertyKeys.size, "1");
     }
 
     public void setSize(String size) {
         getStateHelper().put(PropertyKeys.size, size);
     }
-    
+
     public String getStyleClass() {
         return (String) getStateHelper().eval(PropertyKeys.styleClass, null);
     }
-    
+
     public void setStyleClass(String styleClass) {
         getStateHelper().put(PropertyKeys.styleClass, styleClass);
     }
-    
+
     public String getOnclick() {
         return (String) getStateHelper().eval(PropertyKeys.onclick, null);
     }
-    
+
     public void setOnclick(String onclick) {
         getStateHelper().put(PropertyKeys.onclick, onclick);
     }
-    
-
-    public void encodeEnd(FacesContext context) throws IOException {
+public void encodeEnd(FacesContext context) throws IOException {
         ResponseWriter writer = context.getResponseWriter();
         String greetText = getGreetText();
         String value = (String) getValue();
-        String size = getSize();
-        String styleClass=getStyleClass();
+        String tagName = "h" + getSize();
+        String styleClass = getStyleClass();
+        String style = getStyle();
 
-        if(size !=null){
-            int headerSize=Integer.parseInt(size);
-            if(headerSize>=1 && headerSize<=6){
-                writer.startElement("h"+headerSize, this);
-                writer.write(greetText + " " + value);
-                writer.endElement("h"+headerSize);
-                return;
-            }
+        writer.startElement(tagName, this);
+        if (styleClass != null) {
+            writer.writeAttribute("class", styleClass, null);
         }
 
-        writer.startElement("span", this);
-        if(styleClass !=null){
-            writer.writeAttribute("class",styleClass,null);
+        if (style != null) {
+            writer.writeAttribute("style", style, null);
         }
-        if(getOnclick() !=null){
-            writer.writeAttribute("onclick",getOnclick(),null);
+
+        if (getOnclick() != null) {
+            writer.writeAttribute("onclick", getOnclick(), null);
         }
-        
+
         writer.write(greetText + " " + value);
+
         writer.endElement("span");
     }
 
